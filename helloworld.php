5eac3e7da1c38d6a7d374966074b70c400914884
wangwenbin <wangwenbin@outlook.com>
2014-05-07 22:07:44 +0800

    modified:   helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -6,7 +6,10 @@ i love apk!
 <?php
 echo 'Hello, world!';
 echo '<br/>';
-echo 'This is iloveapk on gae, thanks for Google!';
+echo 'This is writeme on gae, thanks for Google!';
+echo '<br/>';
+echo '<a herf="deefi.com">deefi.com</a>';
+echo '<br/>';
 ?>
 <a herf="deefi.com">deefi.com</a>
 </body>
