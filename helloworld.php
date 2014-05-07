a2bb04bf62c7e5a5e162c0bc233fe0351aabd91b
wangwenbin <wangwenbin@outlook.com>
2014-05-07 21:57:20 +0800

    On branch master  Initial commit
     Changes to be committed:
    	new file:   app.yaml
    	new file:   helloworld.php
    	new file:   iloveapk.php
    	new file:   robots.txt

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+i love apk!
+</head>
+<body>
+<?php
+echo 'Hello, world!';
+echo '<br/>';
+echo 'This is iloveapk on gae, thanks for Google!';
+?>
+<a herf="deefi.com">deefi.com</a>
+</body>
+</html>
---
+++ iloveapk.php
@@ -0,0 +1,4 @@
+<?php
+echo "this is a php file";
+phpinfo();
+?>
